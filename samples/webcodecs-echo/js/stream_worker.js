2690e273cfa919eabdf805b45c15797936d74814
Bernard Aboba <bernard.aboba@gmail.com>
2023-09-30 11:58:26 -0700

    Fix optimizeForLatency

--- samples/webcodecs-echo/js/stream_worker.js
+++ samples/webcodecs-echo/js/stream_worker.js
@@ -719,7 +719,7 @@ SSRC = this.config.ssrc
              if (cfg.decoderConfig) {
                // self.postMessage({text: 'Decoder reconfig!'});
                cfg.decoderConfig.hardwareAcceleration = config.decHwAcceleration;
-               cfg.decoderConfig.optimizeForLatency = !(config.latencyPref == 'quality');
+               cfg.decoderConfig.optimizeForLatency = (config.latencyPref == "realtime");
                const decoderConfig = JSON.stringify(cfg.decoderConfig);
                // self.postMessage({text: 'Decoder configuration: ' + decoderConfig});
                const configChunk =
