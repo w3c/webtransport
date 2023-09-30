7f4155c1385775a1e3330a9d8d2092010aa4548d
Bernard Aboba <bernard.aboba@gmail.com>
2023-09-30 11:22:52 -0700

    Condense setting optimizeForLatency

--- samples/webcodecs-echo/js/stream_worker.js
+++ samples/webcodecs-echo/js/stream_worker.js
@@ -719,8 +719,7 @@ SSRC = this.config.ssrc
              if (cfg.decoderConfig) {
                // self.postMessage({text: 'Decoder reconfig!'});
                cfg.decoderConfig.hardwareAcceleration = config.decHwAcceleration;
-               cfg.decoderConfig.optimizeForLatency = true;
-               if (config.latencyPref == 'quality') cfg.decoderConfig.optimizeForLatency = false;
+               cfg.decoderConfig.optimizeForLatency = !(config.latencyPref == 'quality');
                const decoderConfig = JSON.stringify(cfg.decoderConfig);
                // self.postMessage({text: 'Decoder configuration: ' + decoderConfig});
                const configChunk =
